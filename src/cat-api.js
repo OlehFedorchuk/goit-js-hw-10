644cfea657e2bbf0e5ebd08bfc4547014e09bf76
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-06-12 00:12:08 +0100

    update

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,38 +1,30 @@
+const BASE_URL = 'https://api.thecatapi.com/v1/breeds';
 async function fetchBreeds() {
-  return fetch(
-    'https://api.thecatapi.com/v1/breeds?api_key=live_kC6jvNAdLNzKFQvhHELbn8x02k6W0wCP14oVDeGVWUCIiRxXvvLo4lfIWq7LpUA6'
-  )
+  return fetch(`${BASE_URL}`)
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Error fetching cat breeds');
+      if (!response.ok) {
+        throw new Error(response.status);
       }
+      return response.json();
     })
-    .then(dataArrayObj => {
-      //   console.log(data);
-      //   return data.map(breed => breed.name);
-      return dataArrayObj;
-    })
-    .catch(error => {
-      console.log(error);
+    .then(data => {
+      return data;
     });
 }
 
 async function fetchCatByBreed(breedId) {
-  const url = `https://api.thecatapi.com/v1/images/search?breed_ids${breedId}`;
-  //   console.log(breedId);
-  return fetch(url)
+  return fetch(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}&api_key=live_kC6jvNAdLNzKFQvhHELbn8x02k6W0wCP14oVDeGVWUCIiRxXvvLo4lfIWq7LpUA6`
+  )
     .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new error('Error retrieving cat information.');
+      if (!response.ok) {
+        throw new Error(response.status);
       }
+      return response.json();
     })
-    .then(data => {
-      console.log(data);
-      return data[0];
+    .then(singleDataBreed => {
+      return singleDataBreed;
     });
 }
+
 export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,27 +1,52 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
-function populateBreedSelect(breeds) {
-  console.log(breeds);
-  const breedSelect = document.getElementById('breed-select');
-
-  breeds.forEach(breed => {
-    const option = document.createElement('option');
-    option.value = breed.id;
-    option.textContent = breed.name;
-    console.log(option);
-    breedSelect.appendChild(option);
-  });
-}
+const selectEl = document.querySelector('.breed-select');
+const divEl = document.querySelector('.cat-info');
+const loaderEl = document.querySelector('.loader');
+const errorEl = document.querySelector('.error');
 
 document.addEventListener('DOMContentLoaded', () => {
+  loaderEl.hidden = false;
+  errorEl.hidden = true;
+
   fetchBreeds()
     .then(breeds => {
       populateBreedSelect(breeds);
+      loaderEl.hidden = true;
+      errorEl.hidden = true;
     })
-    .catch(error => {
-      console.error(error);
-    });
+    .catch(error => console.log(error));
+
+  selectEl.addEventListener('change', () => {
+    loaderEl.hidden = false;
+    errorEl.hidden = true;
+
+    const value = getValue();
+    fetchCatByBreed(value)
+      .then(data => {
+        displayCatInfo(data);
+        loaderEl.hidden = true;
+        errorEl.hidden = true;
+      })
+      .catch(error => {
+        errorEl.hidden = true;
+      });
+  });
 });
 
-const id = 'ozEvzdVM-';
-fetchCatByBreed(id);
+function populateBreedSelect(breeds) {
+  const markup = breeds.map(breed => {
+    return `<option value=${breed.id}>${breed.name}</option>`;
+  });
+  selectEl.innerHTML = markup;
+}
+
+function getValue() {
+  const selectedOption = selectEl.options[selectEl.selectedIndex];
+  const selectedValue = selectedOption.value;
+  return selectedValue;
+}
+
+function displayCatInfo(data) {
+  divEl.innerHTML = `<img class='img' src="${data[0].url}" alt="cat" width="350" height="350"><div class = 'x'><h3>${data[0].breeds[0].name}</h3><p>${data[0].breeds[0].description}</p><p><span class='Accent'>Temperament:</span> ${data[0].breeds[0].temperament}</p></div>`;
+}

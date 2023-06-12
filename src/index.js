6621c4acfd607fd0ac7350a22b8b40cf3d268e4e
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2023-06-13 00:15:25 +0100

    update

--- src/index.js
+++ src/index.js
@@ -7,13 +7,11 @@ const errorEl = document.querySelector('.error');
 
 document.addEventListener('DOMContentLoaded', () => {
   loaderEl.hidden = false;
-  // errorEl.hidden = true;
 
   fetchBreeds()
     .then(breeds => {
       populateBreedSelect(breeds);
       loaderEl.hidden = true;
-      // errorEl.hidden = true;
     })
     .catch(error => console.log(error));
 
@@ -22,13 +20,17 @@ document.addEventListener('DOMContentLoaded', () => {
     errorEl.hidden = true;
 
     const value = getValue();
-    fetchCatByBreed(value).then(data => {
-      if (data.length === 0) {
-        reset();
-      }
-      displayCatInfo(data);
-      loaderEl.hidden = true;
-    });
+    fetchCatByBreed(value)
+      .then(data => {
+        if (data.length === 0) {
+          reset();
+        }
+        displayCatInfo(data);
+        loaderEl.hidden = true;
+      })
+      .catch(() => {
+        throw new Error('Oops! Something went wrong! Try reloading the page!');
+      });
   });
 });
 
@@ -49,7 +51,6 @@ function displayCatInfo(data) {
   divEl.innerHTML = `<img class='img' src="${data[0].url}" alt="cat" width="350" height="350"><div class = 'x'><h3>${data[0].breeds[0].name}</h3><p>${data[0].breeds[0].description}</p><p><span class='Accent'>Temperament:</span> ${data[0].breeds[0].temperament}</p></div>`;
 }
 function reset() {
-  console.log('data is empty');
   loaderEl.hidden = true;
   errorEl.hidden = false;
   divEl.innerHTML = ``;
